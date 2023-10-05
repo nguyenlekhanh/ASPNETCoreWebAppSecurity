ec072d34bc66f1191ef8e614a5cedc01d7fd8b37
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-10-04 19:58:46 -0500

    implement login form, just login form

---
+++ ASPNETCoreWebAppSecurity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETCoreWebAppSecurity.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+
+        public void OnGet()
+        {
+            this.Credential = new Credential();
+            Credential.UserName = "admin";
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+
+    public class Credential
+    {
+
+        [Required]
+        [Display(Name ="User Name")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
